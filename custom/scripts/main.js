c83bd428840f47b3255f07709fd956005190c206
manuelmsni <manuelmsni@hotmail.com>
2021-10-18 22:42:34 +0200

    Add files via upload

--- custom/scripts/main.js
+++ custom/scripts/main.js
@@ -2,3 +2,17 @@ function ShowHide(ShowClass, HideClass) {
   $(`.${ShowClass}`).fadeIn('slow');
   $(`.${HideClass}`).hide();
 }
+
+function ImgSrcDescription(ThisImgSRC, ImgDesc) {
+  var ModImg = document.getElementById("ModalImg");
+  var Desc = ImgDesc;
+  $("body").css('overflow','hidden');
+  ModImg.src = ThisImgSRC;
+  $("#ModalDesc").text(Desc);
+  $("#ImgModal").fadeIn('slow');
+}
+
+function CloseModal() {
+  $("body").css('overflow','visible');
+  $("#ImgModal").hide();
+}
